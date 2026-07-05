9c354a5f35a159ca63d44aae328694157b381d26
Jay Krishnan <jaykrishnan1997@gmail.com>
2026-07-05 20:57:57 +0200

    beginning mzegen

---
+++ mazegen/maze_generator.py
@@ -0,0 +1,17 @@
+class MazeGenerator:
+    def __init__(self, width: int, height: int, entry_coord: tuple[int, int], exit_coord: tuple[int, int], perfect: bool, seed: int | None):
+        self._width = width
+        self._height = height
+        self._entry_coord = entry_coord
+        self._exit_coord = exit_coord
+        self._perfect = perfect
+        self._seed = seed
+        self._grid = [[15] * width for _ in range(height)]
+
+    def get_grid(self) -> list[list[int]]:
+        return [row[:] for row in self._grid]
+
+maze = MazeGenerator(3, 3, (0, 0), (2, 2), True, None)
+grid = maze.get_grid()
+grid[0][0] = 99
+print(maze.get_grid())
